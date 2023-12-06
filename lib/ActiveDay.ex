5819dddee175d6c43173135b80f3c6386e1b28e6
Blaž Peterlin <blasius.peterlin@gmail.com>
2023-12-06 23:39:50 +0100

    Day 01 part 01

--- lib/ActiveDay.ex
+++ lib/ActiveDay.ex
@@ -1,6 +1,17 @@
 defmodule ActiveDay do
   def solve1 do
-    x = :world
-    x
+    {:ok, input} = File.read("lib/input.txt");
+    lines = String.split(input, "\r\n")
+    tokenized =
+      lines
+      |> Enum.filter(fn x -> x != "" end)
+      |> Enum.map(fn x -> (Regex.scan(~r/\d/, x)) |> Enum.concat() |> (Enum.map (&String.to_integer/1)) end) #
+    res =
+      tokenized
+      |> Enum.map(fn tkns -> (hd tkns)*10 + (tkns |> Enum.reverse |> hd) end)
+      |> Enum.sum()
+
+    # x = lines
+    res
   end
 end
